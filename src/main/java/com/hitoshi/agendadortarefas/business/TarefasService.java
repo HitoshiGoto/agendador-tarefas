5c38edad5bb0c76d719e606bf365ab1a50392502
Hitoshi <hitoshigoto@outlook.com>
2026-04-23 16:31:58 -0300

    adicionados métodos de delete, patch de status e put de tarefas

--- src/main/java/com/hitoshi/agendadortarefas/business/TarefasService.java
+++ src/main/java/com/hitoshi/agendadortarefas/business/TarefasService.java
@@ -2,13 +2,14 @@ package com.hitoshi.agendadortarefas.business;
 
 
 import com.hitoshi.agendadortarefas.business.dto.TarefasDTO;
+import com.hitoshi.agendadortarefas.business.mapper.TarefaUpdateConverter;
 import com.hitoshi.agendadortarefas.business.mapper.TarefasConverter;
 import com.hitoshi.agendadortarefas.infrastructure.entity.TarefasEntity;
 import com.hitoshi.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
+import com.hitoshi.agendadortarefas.infrastructure.exceptions.ResourceNotFoundException;
 import com.hitoshi.agendadortarefas.infrastructure.repository.TarefasRepository;
 import com.hitoshi.agendadortarefas.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.text.similarity.JaroWinklerDistance;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -21,8 +22,9 @@ public class TarefasService {
     private final TarefasRepository tarefasRepository;
     private final TarefasConverter tarefaConverter;
     private final JwtUtil jwtUtil;
+    private final TarefaUpdateConverter tarefaUpdateConverter;
 
-    public TarefasDTO gravarTarefa(String token, TarefasDTO dto){
+    public TarefasDTO gravarTarefa(String token, TarefasDTO dto) {
         String email = jwtUtil.extrairEmailToken(token.substring(7));
 
         dto.setDataCriacao(LocalDateTime.now());
@@ -33,14 +35,14 @@ public class TarefasService {
         return tarefaConverter.paraTarefaDTO(tarefasRepository.save(entity));
     }
 
-    public List<TarefasDTO> buscaTarefasAgendadasPorPeriodo(LocalDateTime dataInicial, LocalDateTime dataFinal){
+    public List<TarefasDTO> buscaTarefasAgendadasPorPeriodo(LocalDateTime dataInicial, LocalDateTime dataFinal) {
 
         return tarefaConverter.paraListaTarefasDTO(
                 tarefasRepository.findByDataEventoBetween(dataInicial, dataFinal));
 
     }
 
-    public List<TarefasDTO> buscaTarefasPorEmail(String token){
+    public List<TarefasDTO> buscaTarefasPorEmail(String token) {
         String email = jwtUtil.extrairEmailToken(token.substring(7));
         List<TarefasEntity> listaTarefas = tarefasRepository.findByEmailUsuario(email);
 
@@ -48,4 +50,36 @@ public class TarefasService {
 
     }
 
+    public void deletaTarefaPorId(String id) {
+        try {
+            tarefasRepository.deleteById(id);
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("Erro ao deletar tarefa por id, id inexistente " + id, e.getCause());
+        }
+
+    }
+
+    public TarefasDTO alteraStatus(StatusNotificacaoEnum status, String id) {
+        try {
+            TarefasEntity entity = tarefasRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Tarefa não encontrada " + id));
+
+            entity.setStatusNotificacaoEnum(status);
+            return tarefaConverter.paraTarefaDTO(tarefasRepository.save(entity));
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("Erro ao alterar status da tarefa " + e.getCause());
+        }
+
+    }
+
+    public TarefasDTO updateTarefas(TarefasDTO dto, String id) {
+        try {
+            TarefasEntity entity = tarefasRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Tarefa não encontrada " + id));
+        tarefaUpdateConverter.updateTarefas(dto, entity);
+        return tarefaConverter.paraTarefaDTO(tarefasRepository.save(entity));
+
+
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("Erro ao alterar status da tarefa " + e.getCause());
+        }
+    }
 }
--- src/main/java/com/hitoshi/agendadortarefas/controller/TarefasController.java
+++ src/main/java/com/hitoshi/agendadortarefas/controller/TarefasController.java
@@ -2,6 +2,7 @@ package com.hitoshi.agendadortarefas.controller;
 
 import com.hitoshi.agendadortarefas.business.TarefasService;
 import com.hitoshi.agendadortarefas.business.dto.TarefasDTO;
+import com.hitoshi.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -38,5 +39,24 @@ public class TarefasController {
     public ResponseEntity<List<TarefasDTO>> buscaTarefasPorEmail(@RequestHeader("Authorization") String token){
         List<TarefasDTO> tarefas = tarefasService.buscaTarefasPorEmail(token);
         return ResponseEntity.ok(tarefas);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletaTarefaPorId(@RequestParam("id") String id) {
+        tarefasService.deletaTarefaPorId(id);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<TarefasDTO> alteraStatusNotificacao(@RequestParam("status")StatusNotificacaoEnum status,
+                                                              @RequestParam("id")String id){
+        return ResponseEntity.ok(tarefasService.alteraStatus(status, id));
+
+    }
+
+    @PutMapping
+    public ResponseEntity<TarefasDTO> updateTarefas(@RequestBody TarefasDTO dto, @RequestParam("id")String id){
+        return ResponseEntity.ok(tarefasService.updateTarefas(dto, id));
     }
 }
